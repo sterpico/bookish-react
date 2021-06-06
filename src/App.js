136e4cd25cd1377a1f0f65efe579181cab46981e
Costas Sterpis <costas.sterpis@gmail.com>
2021-06-07 00:42:07 +0100

    add stubserver, refactor to component, use hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Typography from '@material-ui/core/Typography'
+import BookList from './BookList';
+import axios from 'axios';
 
-function App() {
+
+
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const res = await axios.get('http://localhost:8080/books');
+      setBooks(res.data);
+    };
+
+    fetchBooks();
+  }, []);
+
   return (
     <div>
-      <Typography variant='h2' component='h2' data-test='heading'>Bookish</Typography>
+      <Typography variant="h2" component="h2" data-test="heading">
+        Bookish
+      </Typography>
+      <BookList books={books} />
     </div>
   );
 }
+
 
 export default App;
